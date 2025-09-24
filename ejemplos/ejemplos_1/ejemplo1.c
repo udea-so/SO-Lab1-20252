ccc46e186c95520948981c0255e82227ff6f90a8
jamartinezv2023 <jose.martinez7@udea.edu.co>
2025-09-24 18:31:35 -0500

    Avance: agregados wgrep, wzip, wunzip, ejemplos y tests

---
+++ ejemplos/ejemplos_1/ejemplo1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void show(int x);
+int add(int x, int y);
+
+int main() {
+  int f = 10;
+  int g = 20;
+  int h = add(f, g);
+  show(h); // to print some output to screen
+  int i = add(2, 3);
+  show(i);
+  return 1;
+}
+
+int add(int x, int y) {
+  return x + y;
+}
+
+void show(int x) {
+  printf("value: %d\n",x);
+}
